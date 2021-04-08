4899df455614bd44a593de7e1507ec085aae1595
Panasenko Vova <panvovasasha@gmail.com>
2021-04-08 20:47:20 +0300

    good work

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from classes.App import App
 from classes.Book import Book
 from classes.Movie import Movie
 from classes.Song import Song
+import requests
 
-def main():
-    with open("files/1.json") as f:
+
+def read_file(file):
+    with open("files/"+file) as f:
         data = json.loads(f.read())
-        print(len(data))
 
     for i in range(0, len(data)):
         type = data[i]['type']
@@ -21,12 +22,27 @@ def main():
         elif type == "book":
             Book(data[i]['data']).print()
 
-        print(type)
+
+def download_file(url, name):
+    r = requests.get(url, allow_redirects=True)
+    open('files/' + name, 'wb').write(r.content)
+
+
+def read_files_list():  # add async
+    with open("files/files_list.data") as file:
+        return file.read().split("\n")
+
+
+def main():
+    download_file('https://data-engineering-interns.macpaw.io/files_list.data', name="files_list.data")
+    for i in read_files_list():
+        print(i)
+        download_file("https://data-engineering-interns.macpaw.io/" + i, name=i)
+        read_file(i)
 
 
 if __name__ == '__main__':
     main()
 
-    # Научиться скачивать файл в эту директорию
-    # считывать все названия и по очереди их проганять через нашу программу
+
     # и сделать сам прогон программы - чтение json файликов
